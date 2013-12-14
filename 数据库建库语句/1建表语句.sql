aeb1878e64e67a11d447a41ede577cc668175134
jxy <czjxy8898@gmail.com>
2013-12-15 02:46:20 +0800

    增加教学计划 入学年份  及 任必限

--- 数据库建库语句/1建表语句.sql
+++ 数据库建库语句/1建表语句.sql
@@ -1,7 +1,7 @@
 USE [学籍管理系统]
 GO
 
-/****** Object:  Table [dbo].[班级]    Script Date: 12/09/2013 01:06:57 ******/
+/****** Object:  Table [dbo].[班级]    Script Date: 12/15/2013 02:28:21 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,6 +12,7 @@ CREATE TABLE [dbo].[班级](
 	[班号] [nchar](6) NOT NULL,
 	[专业号] [int] NOT NULL,
 	[班主任] [nchar](10) NOT NULL,
+	[入学年份] [int] NOT NULL,
  CONSTRAINT [PK_班级] PRIMARY KEY CLUSTERED 
 (
 	[班号] ASC
@@ -26,6 +27,15 @@ GO
 
 ALTER TABLE [dbo].[班级] CHECK CONSTRAINT [FK_班级_专业]
 GO
+
+ALTER TABLE [dbo].[班级]  WITH CHECK ADD  CONSTRAINT [CK_班级] CHECK  (([入学年份]>(1800) AND [入学年份]<(10000)))
+GO
+
+ALTER TABLE [dbo].[班级] CHECK CONSTRAINT [CK_班级]
+GO
+
+
+
 
 
 USE [学籍管理系统]
@@ -120,7 +130,10 @@ GO
 USE [学籍管理系统]
 GO
 
-/****** Object:  Table [dbo].[课程]    Script Date: 12/09/2013 01:07:29 ******/
+USE [学籍管理系统]
+GO
+
+/****** Object:  Table [dbo].[课程]    Script Date: 12/15/2013 02:43:48 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -131,6 +144,7 @@ CREATE TABLE [dbo].[课程](
 	[课程号] [nchar](9) NOT NULL,
 	[课程名] [nchar](25) NOT NULL,
 	[学分] [int] NOT NULL,
+	[课程类型] [nchar](2) NOT NULL,
  CONSTRAINT [PK_课程] PRIMARY KEY CLUSTERED 
 (
 	[课程号] ASC
@@ -138,6 +152,18 @@ CREATE TABLE [dbo].[课程](
 ) ON [PRIMARY]
 
 GO
+
+ALTER TABLE [dbo].[课程]  WITH CHECK ADD  CONSTRAINT [CK_课程] CHECK  (([课程类型]='任选' OR [课程类型]='限选' OR [课程类型]='必修'))
+GO
+
+ALTER TABLE [dbo].[课程] CHECK CONSTRAINT [CK_课程]
+GO
+
+ALTER TABLE [dbo].[课程] ADD  CONSTRAINT [DF_课程_课程类型]  DEFAULT (N'必修') FOR [课程类型]
+GO
+
+
+
 
 USE [学籍管理系统]
 GO
@@ -200,4 +226,49 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
+
+USE [学籍管理系统]
+GO
+
+/****** Object:  Table [dbo].[教学计划]    Script Date: 12/15/2013 02:23:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[教学计划](
+	[年级] [int] NOT NULL,
+	[专业号] [int] NOT NULL,
+	[课程号] [nchar](9) NOT NULL,
+ CONSTRAINT [PK_教学计划] PRIMARY KEY CLUSTERED 
+(
+	[年级] ASC,
+	[专业号] ASC,
+	[课程号] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[教学计划]  WITH CHECK ADD  CONSTRAINT [FK_教学计划_课程] FOREIGN KEY([课程号])
+REFERENCES [dbo].[课程] ([课程号])
+GO
+
+ALTER TABLE [dbo].[教学计划] CHECK CONSTRAINT [FK_教学计划_课程]
+GO
+
+ALTER TABLE [dbo].[教学计划]  WITH CHECK ADD  CONSTRAINT [FK_教学计划_专业] FOREIGN KEY([专业号])
+REFERENCES [dbo].[专业] ([专业号])
+GO
+
+ALTER TABLE [dbo].[教学计划] CHECK CONSTRAINT [FK_教学计划_专业]
+GO
+
+ALTER TABLE [dbo].[教学计划]  WITH CHECK ADD  CONSTRAINT [CK_教学计划] CHECK  (([年级]>(0) AND [年级]<(5)))
+GO
+
+ALTER TABLE [dbo].[教学计划] CHECK CONSTRAINT [CK_教学计划]
+GO
+
 
